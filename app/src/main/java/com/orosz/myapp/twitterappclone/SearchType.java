16e45110514200141f5165d965b850c9e1bbf79f
Orosz <oroszandrei@gmail.com>
2018-09-29 23:22:44 +0300

    Initial commit

---
+++ app/src/main/java/com/orosz/myapp/twitterappclone/SearchType.java
@@ -0,0 +1,8 @@
+package com.orosz.myapp.twitterappclone;
+
+
+public class SearchType {
+    public static int MyFollowing=1;
+    public static int OnePerson=2;
+    public static int SearchIn=3;
+}
